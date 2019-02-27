b45d38738bb86a4ffc493b93a3224ab841bcd124
fariz.tumbuan <fariz.tumbuan@prosa.ai>
2019-02-27 17:36:54 +0700

    Implemented file.py

--- src/utilities/file.py
+++ src/utilities/file.py
@@ -1,5 +1,14 @@
 def read_byte(filedir):
-    pass
+    file_bytes = bytes('', 'utf-8')
+    try:
+        with open(filedir, 'rb') as f:
+            for line in f.readlines():
+                file_bytes += line
+    except FileNotFoundError:
+        print('[src.utilities.file.read_byte] file not found; returning empty string')
+        return ''
+    return file_bytes
 
-def write_byte(filedir):
-    pass
+def write_byte(data, output_dir):
+    with open(output_dir, 'wb+') as f:
+        f.write(data)
