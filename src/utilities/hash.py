933801110a83ad5eee3ae42f16f243c400ad22f5
fariz.tumbuan <fariz.tumbuan@prosa.ai>
2019-02-28 17:00:51 +0700

    Now handles byte input

--- src/utilities/hash.py
+++ src/utilities/hash.py
@@ -1,21 +1,31 @@
 import hashlib
 
-def sha256(key_string, numeral='byte'):
+def sha256(input, numeral='byte'):
+    try:
+        input = input.encode('utf-8')
+    except AttributeError:
+        pass
+
     if (numeral == 'byte'):
-        hash_result = hashlib.sha256(key_string.encode()).digest()
+        hash_result = hashlib.sha256(input).digest()
     elif (numeral == 'hex'):
-        hash_result = hashlib.sha256(key_string.encode()).hexdigest()
+        hash_result = hashlib.sha256(input).hexdigest()
     else:
         print('Numeral type not found; using bytes')
-        hash_result = hashlib.sha256(key_string.encode()).digest()
+        hash_result = hashlib.sha256(input).digest()
     return hash_result
 
-def md5(key_string, numeral='byte'):
+def md5(input, numeral='byte'):
+    try:
+        input = input.encode('utf-8')
+    except AttributeError:
+        pass
+        
     if (numeral == 'byte'):
-        hash_result = hashlib.md5(key_string.encode()).digest()
+        hash_result = hashlib.md5(input).digest()
     elif (numeral == 'hex'):
-        hash_result = hashlib.md5(key_string.encode()).hexdigest()
+        hash_result = hashlib.md5(input).hexdigest()
     else:
         print('Numeral type not found; using bytes')
-        hash_result = hashlib.md5(key_string.encode()).digest()
+        hash_result = hashlib.md5(input).digest()
     return hash_result
