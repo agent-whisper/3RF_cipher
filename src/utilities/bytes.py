ed80b8934abba40df1116f045a5153e2125575f8
fariz.tumbuan <fariz.tumbuan@prosa.ai>
2019-02-28 17:01:15 +0700

    Implemented byte xor byte operation

--- src/utilities/bytes.py
+++ src/utilities/bytes.py
@@ -11,3 +11,10 @@ def merge_bytes(byte_arr):
     for b in byte_arr:
         result += b
     return result
+
+def xor(byte_a, byte_b):
+    xor_result = b''
+    for a, b in zip(byte_a, byte_b):
+        c = chr(a ^ b)
+        xor_result += bytes(c, 'utf-8')
+    return xor_result
