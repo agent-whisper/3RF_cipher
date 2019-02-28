be4784155863ec205190ed3b534ae0941e970aad
fariz.tumbuan <fariz.tumbuan@prosa.ai>
2019-02-28 17:02:20 +0700

    Implemented encrypy and decrypt for cbc

--- src/blockcipher/modes.py
+++ src/blockcipher/modes.py
@@ -1,4 +1,4 @@
-from src.utilities import file as fl, bytes as by
+from src.utilities import file as fl, bytes as by, hash as hs
 from  src.blockcipher.modules.feistel import Feistel
 
 class ElectronicCodeBook():
@@ -27,13 +27,34 @@ class ElectronicCodeBook():
 class CipherBlockChaining():
     @staticmethod
     def encrypt(filedir, ext_key, output_filename):
-        pass
+        plaintext = fl.read_byte(filedir)
+        plaintext_blocks = by.generate_slices(plaintext, slice_length=16)
+        block_chain = hs.md5(ext_key)
+        
+        ciphertext_blocks = []
+        for pb in plaintext_blocks:
+            pb_xor = by.xor(pb, block_chain)
+            cb = Feistel.encrypt(pb_xor, ext_key)
+            block_chain = cb
+            ciphertext_blocks.append(cb)
+        ciphertext = by.merge_bytes(ciphertext_blocks)
+        return ciphertext
     
     @staticmethod
     def decrypt(filedir, ext_key, output_filename):
-        pass
+        ciphertext = fl.read_byte(filedir)
+        ciphertext_blocks = by.generate_slices(ciphertext, slice_length=16)
+        block_chain = hs.md5(ext_key)
 
-
+        plaintext_block = []
+        for cb in ciphertext_blocks:
+            pb_xor = Feistel.decrypt(cb, ext_key)
+            block_chain = cb
+            pb = by.xor(pb_xor, block_chain)
+            plaintext_block.append(pb)
+        plaintext = by.merge_bytes(plaintext_block)
+        print(plaintext)
+        return plaintext
 
 class CipherFeedback():
     @staticmethod
