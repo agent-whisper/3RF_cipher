05388f75df5c0f556fd2e304b0c034db08b99dc8
fariz.tumbuan <fariz.tumbuan@prosa.ai>
2019-03-07 07:41:10 +0700

    Refactored key scheduling scheme to its own module

--- src/blockcipher/key/scheduling.py
+++ src/blockcipher/key/scheduling.py
@@ -1,7 +1,8 @@
 from src.utilities import hash, bytes
 
-def generate_rk(key_string):
-    pass
-
-def generate_secondary_rk(round_key):
-    pass
+def generate_round_keys(ext_key, division_val):
+    ext_key = hash.sha256(ext_key)
+    round_keys = []
+    for key_parts in bytes.generate_slices(ext_key, slice_length=division_val):
+        round_keys.append(hash.sha256(key_parts))
+    return round_keys
--- src/blockcipher/modules/feistel.py
+++ src/blockcipher/modules/feistel.py
@@ -1,32 +1,26 @@
 from src.blockcipher.modules import round_functions
 from src.utilities import hash
-
+from src.blockcipher.key import scheduling
 
 class Feistel():
 	@staticmethod
 	def encrypt(data_block, ext_key):
-		ext_key = hash.sha256(ext_key)
-		len_round_key = len(ext_key) // 16
+		round_keys = scheduling.generate_round_keys(ext_key, 2)
 
 		for round_idx in range (0,16) :
-			ext_round_key = ext_key[round_idx*len_round_key:(round_idx+1)*len_round_key]
-			round_key = hash.sha256(ext_round_key)
 			combination = 0
-			for char in round_key :
+			for char in round_keys[round_idx] :
 				combination = (combination + char) % 6
-			data_block = round_functions.rf_combination(data_block, round_key, combination)
+			data_block = round_functions.rf_combination(data_block, round_keys[round_idx], combination)
 		return data_block
 
 	@staticmethod
 	def decrypt(data_block, ext_key):
-		ext_key = hash.sha256(ext_key)
-		len_round_key = len(ext_key) // 16
+		round_keys = scheduling.generate_round_keys(ext_key, 2)
 
 		for round_idx in range (15,-1,-1) :
-			ext_round_key = ext_key[round_idx*len_round_key:(round_idx+1)*len_round_key]
-			round_key = hash.sha256(ext_round_key)
 			combination = 0
-			for char in round_key :
+			for char in round_keys[round_idx] :
 				combination = (combination + char) % 6
-			data_block = round_functions.rf_inv_combination(data_block, round_key, combination)
+			data_block = round_functions.rf_inv_combination(data_block, round_keys[round_idx], combination)
 		return data_block
